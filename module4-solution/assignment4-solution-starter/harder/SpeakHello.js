f8ee917a766e98eaae4986ac4bc055d86a6fa8fd
ptyagi <ptyagi@thoughtworks.com>
2017-03-18 19:18:39 +0530

    Solution for assignment 4

---
+++ module4-solution/assignment4-solution-starter/harder/SpeakHello.js
@@ -0,0 +1,13 @@
+(function(window){
+// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+var helloSpeaker = {};
+var speakWord = "Hello";
+
+// STEP 4: Rewrite the 'speak' function such that it is attached to the
+// helloSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+helloSpeaker.speak=function (name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
+})(window);
---
+++ module4-solution/assignment4-solution-starter/harder/script.js
@@ -0,0 +1,12 @@
+(function(){var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (name in names) {
+  var firstLetter = names[name].charAt(0);
+
+  if (firstLetter.toLowerCase() == 'j') {
+    byeSpeaker.speak(names[name]);
+  } else {
+    helloSpeaker.speak(names[name]);
+  }
+}})();
+
